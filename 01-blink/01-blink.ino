96164e70f8c928dc6bc97d38b3d5e9fb5c69cc32
gnu <gounane.said@gmail.com>
2021-01-15 20:38:51 +0100

    serial

--- 01-blink/01-blink.ino
+++ 01-blink/01-blink.ino
@@ -1,11 +1,13 @@
 
 int LED = 2;
 void setup() {
+  Serial.begin(115200);
   pinMode(LED,OUTPUT);
 
 }
 
 void loop() {
+  Serial.print("...");
   digitalWrite(LED,HIGH);
   delay(1000);
   digitalWrite(LED,LOW);
